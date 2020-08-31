b3835a7c75075ec1ffe68a9e5f9995a2ec50a36b
Soundna <soundna@Soundnas-MacBook-Pro.local>
2020-08-31 12:44:54 -0400

    add searchbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css'
 import Header from './Header'
+import Search from './ToolBar/Search'
 
 class App extends React.Component {
   render() {
     return (
+      <div>
       <Header />
+      <Search />
+      </div>
     );
   }
 }
---
+++ src/ToolBar/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Search(){
+    return <div className = "search-container">
+        <input label = "Search" className= "search-bar" type="text" placeholder="search"></input>
+        <button>search</button>
+    </div>
+}
+
+
+
+export default Search
